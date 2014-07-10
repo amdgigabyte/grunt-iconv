7ea6d0d9414bd68c0a587b6779bead7b1c7ebafd
noyobo <nongyoubao@alibaba-inc.com>
2014-07-10 14:18:46 +0800

    get oprions changes

--- tasks/iconv.js
+++ tasks/iconv.js
@@ -15,10 +15,14 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('iconv', 'change file encoding', function() {
 
         var f_charset,t_charset;
+		
+		var options = this.options({
+		    from_charset :'',
+		    to_charset : ''
+		});
 
-        this.data.options = this.data.options || {};
-        f_charset = this.data.options.from_charset || 'utf8';
-        t_charset = this.data.options.to_charset || 'utf8';
+        f_charset = options.from_charset || 'utf8';
+        t_charset = options.to_charset || 'utf8';
 
         this.files.forEach(function(files){
             files.src.forEach(function(file){
